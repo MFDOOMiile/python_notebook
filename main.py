e2f9e6324c34546a0df1217412b8a5a5e4efa79f
rafi.alvi821 <rafi.alvi821@gmail.com>
2019-05-28 14:19:14 -0400

    made the content dynamic

--- main.py
+++ main.py
@@ -9,19 +9,15 @@ A note will have 3 parts:
 - Date/Time
 
 """
-
-notebook = []
-
 counter = 1
-note_id = counter
-
-counter += 1
-
+notebook = []
 now = date.today()
 
-content = "This is content"
-note = (note_id, str(now), content)
-
-notebook.append(note)
-print(notebook)
+while counter <= 5:
+    content = input("What is the note? \n> ")
+    note_id = counter
+    note = (note_id, str(now), content)
+    notebook.append(note)
+    counter += 1
+    print(notebook)
 
